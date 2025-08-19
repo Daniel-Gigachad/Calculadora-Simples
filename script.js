fc9b59c9d5e61d836cf663b97bc7db067f3dd32c
DANIEL DA SILVA ARGOLO <daniel.argolo@cs.cruzeirodosul.edu.br>
2025-08-19 11:44:15 -0300

    Troca de cores de botões, adição de botão +/- e raiz quadrada, botão C agora funciona para deletar todo o conteúdo e foi adicionado o placeholder 0 no visor

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function add(valor) {
     document.getElementById("visor").value += valor;
 }
 
+//function that makes the ± button work (partially)
 function invert() {
     document.getElementById("visor").value = visor.value*-1;
 }
@@ -19,6 +20,7 @@ function removeLastDigit() {
     document.getElementById("visor").value = visor.value.slice(0, -1);
 }
 
+//function for the C button to work
 function removeAll() {
     document.getElementById("visor").value = visor.value.slice(0, null);
 }
